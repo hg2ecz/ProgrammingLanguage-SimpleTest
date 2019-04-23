7d65f7b9b31b5dcc00bf4b5328ca53b903e9703e
Zsolt Krüpl <hg2ecz@ham.hu>
2019-04-23 08:33:07 +0200

    tnx csortu@hup

---
+++ forloop-add-test/calconly_forloop_add_test.r
@@ -0,0 +1,14 @@
+#!/usr/bin/env Rscript
+
+main <- function(toval) {
+    sumval <- 0
+    for (i in 0:(toval-1)){
+        for (j in 0:(toval-1)){
+            sumval <- sumval + i + (j^1)
+        }
+    }
+    print(sumval)
+}
+
+args <- commandArgs(trailingOnly=TRUE)
+main( as.numeric(args[1]) )
