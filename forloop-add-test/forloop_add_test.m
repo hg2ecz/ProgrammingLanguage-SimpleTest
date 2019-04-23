648c51f730aa3be4816ea08f21ad2aaafe0394e3
Zsolt Krüpl <hg2ecz@ham.hu>
2019-04-23 09:10:29 +0200

    simple tests

---
+++ forloop-add-test/forloop_add_test.m
@@ -0,0 +1,15 @@
+#!/usr/bin/env octave
+
+felesleges_de_kell_ide_egy_ertekadas = 0;
+
+function main(toval)
+    sumval = 0;
+    for i = 0 : toval-1
+        for j = 0 : toval-1
+            sumval = sumval + i+(j^1);
+        endfor
+    endfor
+    printf("Octave: %d\n", sumval);
+end
+
+main( str2num(argv(){1}) );
